fe7ab01095a20b2d627d5a47d601124d9065a502
Isaac Evavold <isaacevavold@gmail.com>
2017-03-27 12:59:47 -0500

    simplify battery indentation

--- src/RegFixer/Main.java
+++ src/RegFixer/Main.java
@@ -21,11 +21,11 @@ class Main {
 
   static void runBattery (TermiteForest forest, RegexNode twig) throws Exception {
     // Print header.
-    System.out.println("\nregex:\t\ttwig:");
-    System.out.printf("%s\t\t%s\n", forest.getTree(), twig);
+    System.out.printf("\n%-20s%s\n", "regex:", "twig:");
+    System.out.printf("%-20s%s\n", forest.getTree(), twig);
     System.out.println("--------------------------------");
 
-    // Graft a new twig at each hole location in each digested tree.
+    // Graft a new twig at the hole location in each digested tree.
     List<RegexNode> graftedTrees = forest.getTrees().stream()
       .map(elem -> Grafter.graft(elem, twig))
       .collect(Collectors.toList());
@@ -34,7 +34,7 @@ class Main {
     for (int i = 0; i < graftedTrees.size(); i++) {
       TermiteTree digestedTree = forest.getTrees().get(i);
       RegexNode graftedTree = graftedTrees.get(i);
-      System.out.printf("%s\t\t%s\n", digestedTree, graftedTree);
+      System.out.printf("%-21s%s\n", digestedTree, graftedTree);
     }
   }
 }
