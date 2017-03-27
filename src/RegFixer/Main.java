afa55e5f40b862f17016df061a8b86683fe18a37
Isaac Evavold <isaacevavold@gmail.com>
2017-03-27 01:49:37 -0500

    an example of conducting branch replacement analysis

--- src/RegFixer/Main.java
+++ src/RegFixer/Main.java
@@ -1,29 +1,47 @@
 package RegFixer;
 
-import java.util.List;
-import RegexParser.RegexNode;
-import RegexParser.RegexParserProvider;
+import java.util.*;
+import java.util.stream.*;
+import RegexParser.*;
 
-public class Main {
+class Main {
   public static void main (String[] args) {
-    if (args.length < 1) {
-      System.err.println("Usage: java RegFixer <regex>");
-      System.exit(1);
-    }
+    runBatteries("ab*c", "x*", "y", "z");
+  }
 
-    // Parse a string to a regex AST.
-    RegexNode regex = (new RegexParserProvider(args[0])).process();
-
-    // Add holes to the AST.
-    List<RegexNode> digestedTrees = Termite.digest(regex);
-
-    // Print holed ASTs.
-    if (digestedTrees == null || digestedTrees.size() == 0) {
-      System.out.println("no trees returned");
-    } else {
-      for (RegexNode tree : digestedTrees) {
-        System.out.println(tree.toCleanString());
+  static void runBatteries (String treeRegex, String... branchRegexes) {
+    for (int i = 0; i < branchRegexes.length; i++) {
+      try {
+        runBattery(treeRegex, branchRegexes[i]);
+      } catch (Exception ex) {
+        ex.printStackTrace();
+        System.exit(1);
       }
+    }
+  }
+
+  static void runBattery (String treeRegex, String branchRegex) throws Exception {
+    RegexNode tree = RegexParser.Main.parse(treeRegex);
+    RegexNode branch = RegexParser.Main.parse(branchRegex);
+
+    // Print header.
+    System.out.println("\nregex:\t\tbranch:");
+    System.out.printf("%s\t\t%s\n", tree, branch);
+    System.out.println("--------------------------------");
+
+    // Add holes to all possible locations in tree.
+    List<DigestedTree> digestedTrees = Termite.digest(tree);
+
+    // Graft a new branch at each hole location in each digested tree.
+    List<RegexNode> graftedTrees = digestedTrees.stream()
+      .map(elem -> Grafter.graft(elem, branch))
+      .collect(Collectors.toList());
+
+    // Print comparison of digested tree with grafted tree.
+    for (int i = 0; i < graftedTrees.size(); i++) {
+      DigestedTree digestedTree = digestedTrees.get(i);
+      RegexNode graftedTree = graftedTrees.get(i);
+      System.out.printf("%s\t\t%s\n", digestedTree, graftedTree);
     }
   }
 }
