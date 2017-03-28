c047de6d9fdbf988fb9295f6b10f4ad91ae3d3cc
Isaac Evavold <isaacevavold@gmail.com>
2017-03-28 18:17:55 -0500

    add method to derive negative examples from comparison of old & new positive examples

--- src/RegFixer/Evaluator.java
+++ src/RegFixer/Evaluator.java
@@ -22,4 +22,34 @@ class Evaluator {
 
     return matches;
   }
+
+  static List<Range> derriveNegativeExamples (List<Range> oldExamples, List<Range> newExamples) {
+    List<Range> negExamples = new LinkedList<Range>();
+
+    Collections.sort(oldExamples);
+    Collections.sort(newExamples);
+
+    outerLoop:
+    for (int i_old = 0, i_new = 0; i_new < newExamples.size() && i_old < oldExamples.size(); i_new++) {
+      Range oldEx = oldExamples.get(i_old++);
+      Range newEx = newExamples.get(i_new);
+
+      // Ignore old ranges that don't intersect the current new range and come
+      // before the current new range.
+      while (oldEx.endsBefore(newEx)) {
+        if (i_old >= oldExamples.size()) {
+          break outerLoop;
+        }
+
+        oldEx = oldExamples.get(i_old++);
+      }
+
+      if (oldEx.startsBefore(newEx) && oldEx.intersects(newEx)) {
+        Range negEx = new Range(newEx.getLeftIndex(), oldEx.getRightIndex());
+        negExamples.add(negEx);
+      }
+    }
+
+    return negExamples;
+  }
 }
