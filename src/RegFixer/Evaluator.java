34f0aefca8150bdf9cb00eee38e593dd815d728c
Isaac Evavold <isaacevavold@gmail.com>
2017-03-31 17:32:53 -0500

    fill out Evaluator class with methods for judging acceptability of candidate regex's

--- src/RegFixer/Evaluator.java
+++ src/RegFixer/Evaluator.java
@@ -7,49 +7,123 @@ import RegexParser.*;
 class Evaluator {
   private String corpus;
 
-  Evaluator (String corpus) {
+  // The indexes in the corpus matched by the original (imperfect) regex.
+  private List<Range> originalRanges;
+
+  // The strings corresponding to the indexes in `originalRanges`.
+  private List<String> originalStrings;
+
+  // The indexes given by the user as the desired results of some better regex.
+  private List<Range> selectedRanges;
+
+  // The strings corresponding to the indexes in `selectedRanges`.
+  private List<String> selectedStrings;
+
+  // Strings that a better regex MUST match.
+  private List<String> positiveExamples;
+
+  // Strings that a better regex MUST reject.
+  private List<String> negativeExamples;
+
+  Evaluator (String corpus, List<Range> originalRanges, List<Range> selectedRanges) {
     this.corpus = corpus;
+
+    this.originalRanges = originalRanges;
+    this.originalStrings = Search.getMatchingStrings(corpus, originalRanges);
+
+    this.selectedRanges = selectedRanges;
+    this.selectedStrings = Search.getMatchingStrings(corpus, selectedRanges);
+
+    this.positiveExamples = this.selectedStrings;
+    this.negativeExamples = new LinkedList<String>(this.originalStrings);
+    this.negativeExamples.removeAll(this.selectedStrings);
   }
 
-  List<Range> test (RegexNode regex) {
-    Matcher mt = Pattern.compile(regex.toString()).matcher(this.corpus);
-    List<Range> matches = new LinkedList<Range>();
-
-    while (mt.find()) {
-      Range match = new Range(mt.start(), mt.end());
-      matches.add(match);
-    }
-
-    return matches;
+  List<Range> getOriginalRanges () {
+    return this.originalRanges;
   }
 
-  static List<Range> derriveNegativeExamples (List<Range> oldExamples, List<Range> newExamples) {
-    List<Range> negExamples = new LinkedList<Range>();
+  List<String> getOriginalStrings () {
+    return this.originalStrings;
+  }
 
-    Collections.sort(oldExamples);
-    Collections.sort(newExamples);
+  List<Range> getSelectedRanges () {
+    return this.selectedRanges;
+  }
 
-    outerLoop:
-    for (int i_old = 0, i_new = 0; i_new < newExamples.size() && i_old < oldExamples.size(); i_new++) {
-      Range oldEx = oldExamples.get(i_old++);
-      Range newEx = newExamples.get(i_new);
+  List<String> getSelectedStrings () {
+    return this.selectedStrings;
+  }
 
-      // Ignore old ranges that don't intersect the current new range and come
-      // before the current new range.
-      while (oldEx.endsBefore(newEx)) {
-        if (i_old >= oldExamples.size()) {
-          break outerLoop;
-        }
+  List<String> getPositiveExamples () {
+    return this.positiveExamples;
+  }
 
-        oldEx = oldExamples.get(i_old++);
-      }
+  List<String> getNegativeExamples () {
+    return this.negativeExamples;
+  }
 
-      if (oldEx.startsBefore(newEx) && oldEx.intersects(newEx)) {
-        Range negEx = new Range(newEx.getLeftIndex(), oldEx.getRightIndex());
-        negExamples.add(negEx);
+  boolean runDotStarTest (TermiteTree tree) {
+    RegexNode twig = new StarNode(new CharDotNode());
+    tree.fillHole(twig);
+    boolean didPass = Evaluator.regexMatchesAllExamples(tree, this.positiveExamples);
+    tree.emptyHole();
+
+    return didPass;
+  }
+
+  /**
+   * TODO: To simulate an "empty set" within the regular expression,
+   * the test inserts some character that is known to not exist within the
+   * corpus and thus cannot expand any regular expression. Currently a
+   * hard-coded "!" character is used instead of a more rigorous approach for
+   * finding a character not in the corpus. Some sort of searching system should
+   * be used to find character candidates instead.
+   */
+  boolean runEmptySetTest (TermiteTree tree) {
+    RegexNode twig = new CharLiteralNode('!');
+    tree.fillHole(twig);
+    boolean didPass = (Evaluator.regexMatchesAllExamples(tree, this.negativeExamples) == false);
+    tree.emptyHole();
+
+    return didPass;
+  }
+
+  boolean runPositiveExampleTest (RegexNode regex) {
+    return Evaluator.regexMatchesAllExamples(regex, this.positiveExamples);
+  }
+
+  boolean runNegativeExampleTest (RegexNode regex) {
+    return (Evaluator.regexMatchesAllExamples(regex, this.negativeExamples) == false);
+  }
+
+  TermiteForest pruneForest (TermiteForest forest) {
+    List<TermiteTree> trees = new LinkedList<TermiteTree>();
+
+    for (TermiteTree tree : forest.getTrees()) {
+      boolean dotStarResult = this.runDotStarTest(tree);
+      boolean emptySetResult = this.runEmptySetTest(tree);
+
+      if (dotStarResult && emptySetResult) {
+        trees.add(tree);
       }
     }
 
-    return negExamples;
+    return new TermiteForest(forest.getTree(), trees);
+  }
+
+  static boolean regexMatchesAllExamples (RegexNode regex, List<String> examples) {
+    // Add start and end regular expression anchors (`^` and `$`) to ensure that
+    // the regular expression is tested against the entire example and not some
+    // substring of the example.
+    Pattern pt = Pattern.compile("^" + regex.toString() + "$");
+
+    for (String example : examples) {
+      if (!pt.matcher(example).matches()) {
+        return false;
+      }
+    }
+
+    return true;
   }
 }
