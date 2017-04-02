0a2f34f85687c07d5da7aa3e46af975033038817
Isaac Evavold <isaacevavold@gmail.com>
2017-04-02 04:29:36 -0500

    custom class for helping generate synthesis reports

---
+++ src/main/java/edu/regfixer/fixer/ReportStream.java
@@ -0,0 +1,46 @@
+package edu.wisc.regfixer.fixer;
+
+import java.io.File;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import edu.wisc.regfixer.parser.RegexNode;
+
+public class ReportStream extends PrintStream {
+  public ReportStream (OutputStream out) {
+    this(out, false);
+  }
+
+  public ReportStream (OutputStream out, boolean useColor) {
+    super(out);
+
+    if (useColor) {
+      Ansi.enableColor();
+    } else {
+      Ansi.disableColor();
+    }
+  }
+
+  public void printHeader (String message) {
+    this.printf("\n%s\n\n", message);
+  }
+
+  public void printRegex (RegexNode regex) {
+    this.printf(Ansi.Cyan.sprintf("  %s\n", regex));
+  }
+
+  public void printMatchStatus (boolean isOK, Range range, String match) {
+    if (isOK) {
+      this.print(Ansi.Green.sprintf("  ✓ %-8s %s\n", range, match));
+    } else {
+      this.printf(Ansi.Red.sprintf("  ✗ %-8s %s\n", range, match));
+    }
+  }
+
+  public void printRegexStatus (boolean isOK, RegexNode regex) {
+    if (isOK) {
+      this.printf(Ansi.Green.sprintf("  ✓ %s\n", regex));
+    } else {
+      this.printf(Ansi.Red.sprintf("  ✗ %s\n", regex));
+    }
+  }
+}
