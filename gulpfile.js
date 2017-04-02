ac754981a85936a52edd03483a9b9d2610860af0
Isaac Evavold <isaacevavold@gmail.com>
2017-04-02 17:16:59 -0500

    move frontend files to new location

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ const del         = require('del')
 // Compile Typescript files to ES6 and combine modules with Rollup.
 gulp.task('compile:js', () => {
   return rollup({
-      entry: './src/typescript/app.ts',
+      entry: './src/main/typescript/app.ts',
       moduleName: 'frontend',
       format: 'iife',
       sourceMap: true,
@@ -34,13 +34,14 @@ gulp.task('compile:js', () => {
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./src/main/resources/dist'))
 })
 
 // Concatenate JS libraries into a single file.
 gulp.task('bundle:js', () => {
   // Paths to libraries used by the frontend. Each file in this list is
-  // concatenated into ./dist/libs.js and will be concatenated in this order:
+  // concatenated into ./src/main/resources/dist/libs.js and will be
+  // concatenated in this order:
   const libs = [
     './node_modules/codemirror/lib/codemirror.js',
     './node_modules/codemirror/addon/display/placeholder.js',
@@ -50,41 +51,42 @@ gulp.task('bundle:js', () => {
 
   return gulp.src(libs)
     .pipe(concat('libs.js'))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./src/main/resources/dist'))
 })
 
 // Uglify app logic and concatenated libraries.
 gulp.task('compress:js', ['compile:js', 'bundle:js'], () => {
-  return gulp.src('./dist/*.js')
+  return gulp.src('./src/main/resources/dist/*.js')
     .pipe(compressJS())
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./src/main/resources/dist'))
 })
 
 // Compile SCSS files to CSS.
 gulp.task('compile:css', () => {
-  return gulp.src('src/sass/*.scss')
+  return gulp.src('src/main/sass/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./src/main/resources/dist'))
 })
 
 // Concatenate CSS stylesheets used by libraries.
 gulp.task('bundle:css', () => {
   // Paths to stylesheets used by the frontend. Each file in this list is
-  // concatenated into ./dist/libs.css and will be concatenated in this order:
+  // concatenated into ./src/main/resources/dist/libs.css and will be
+  // concatenated in this order:
   const libs = [
     './node_modules/codemirror/lib/codemirror.css'
   ]
 
   return gulp.src(libs)
     .pipe(concat('libs.css'))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./src/main/resources/dist'))
 })
 
 // Compress app & library stylesheets.
 gulp.task('compress:css', ['compile:css', 'bundle:css'], () => {
-  return gulp.src('./dist/*.css')
+  return gulp.src('./src/main/resources/dist/*.css')
     .pipe(compressCSS())
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./src/main/resources/dist'))
 })
 
 gulp.task('build:all', ['compress:css', 'compress:js'])
@@ -97,7 +99,8 @@ gulp.task('watch:css', ['compile:css'], () => {
   gulp.watch('src/sass/*.scss', ['compile:css'])
 })
 
-// Delete everything inside the ./dist directory including the directory itself.
+// Delete everything inside the ./src/main/resources/dist directory including
+// the directory itself.
 gulp.task('clean', () => {
-  return del([ './dist' ])
+  return del([ './src/main/resources/dist/*.{css,js,map}' ])
 })
