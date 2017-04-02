403cbb46f77123fd11acd4020f4e1121fe98d4df
Isaac Evavold <isaacevavold@gmail.com>
2017-04-01 19:10:13 -0500

    add compound regex node adviser

---
+++ src/RegFixer/CompoundNodeAdviser.java
@@ -0,0 +1,16 @@
+package regfixer;
+
+import java.util.*;
+import RegexParser.*;
+
+public class CompoundNodeAdviser {
+  private PriorityQueue<RegexNode> suggestions;
+
+  public CompoundNodeAdviser () {
+    // ...
+  }
+
+  public RegexNode nextSuggestion () {
+    return null;
+  }
+}
