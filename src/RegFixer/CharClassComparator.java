3b7f0328d83f0dbc72537d07ba3b7f361540a57b
Isaac Evavold <isaacevavold@gmail.com>
2017-04-01 19:09:27 -0500

    replace suggestion engine with comparator

---
+++ src/RegFixer/CharClassComparator.java
@@ -0,0 +1,11 @@
+package regfixer;
+
+import java.util.*;
+import RegexParser.*;
+
+public class CharClassComparator implements Comparator<CharClass> {
+  @Override
+  public int compare (CharClass a, CharClass b) {
+    return 0;
+  }
+}
