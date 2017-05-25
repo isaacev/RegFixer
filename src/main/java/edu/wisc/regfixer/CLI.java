163c8a94a1dfecd4f5300eebae548f5cb21f3c23
Isaac Evavold <isaacevavold@gmail.com>
2017-05-25 13:43:24 -0500

    support command-line flags

--- src/main/java/edu/wisc/regfixer/CLI.java
+++ src/main/java/edu/wisc/regfixer/CLI.java
@@ -9,14 +9,14 @@ import edu.wisc.regfixer.util.ReportStream;
 public class CLI {
   public static void main (String[] args) {
     if (args.length < 1) {
-      System.err.println("Usage: regfixer <benchmark>");
+      System.err.println("Usage: regfixer [options] <benchmark>");
       System.exit(1);
     }
 
     Job job = null;
 
     try {
-      job = Benchmark.readFromFile(args[0]);
+      job = Benchmark.readFromFile(args[args.length - 1]);
     } catch (IOException ex) {
       System.err.println("unable to read benchmark file");
       System.exit(1);
@@ -24,5 +24,15 @@ public class CLI {
 
     ReportStream report = new ReportStream(System.out, true);
     RegFixer.fix(job, report);
+  }
+
+  private static boolean hasFlag (String flag, String[] args) {
+    for (int i = 0; i < args.length; i++) {
+      if (args[i].equals(flag)) {
+        return true;
+      }
+    }
+
+    return false;
   }
 }
