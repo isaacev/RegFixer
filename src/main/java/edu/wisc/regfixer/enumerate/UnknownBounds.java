d2f440dc8a2d875b4c0cfd581162cbacb9b7e577
isaacev <isaacevavold@gmail.com>
2017-11-04 14:38:40 -0500

    use static variable to set temporary unknown fill

--- src/main/java/edu/wisc/regfixer/enumerate/UnknownBounds.java
+++ src/main/java/edu/wisc/regfixer/enumerate/UnknownBounds.java
@@ -5,15 +5,15 @@ import edu.wisc.regfixer.parser.Bounds;
 public class UnknownBounds extends Bounds implements Unknown {
   // SEE: UnknownChar
   private static Bounds fill = null;
-  private static void setFill () { this.setFill(Bounds.atLeat(0)); }
-  private static void setFill (Bounds bounds) { UnknownBounds.bounds = bounds; }
-  private static void clearFill () { UnknownBounds.bounds = null; }
+  public static void setFill () { UnknownBounds.setFill(Bounds.atLeast(0)); }
+  public static void setFill (Bounds bounds) { UnknownBounds.bounds = bounds; }
+  public static void clearFill () { UnknownBounds.bounds = null; }
 
   private UnknownId id;
 
   public UnknownBounds () {
     super(0, null);
-    this.id = new UnknownId();
+    this.id = new UnknownId(this);
   }
 
   public UnknownId getId () {
