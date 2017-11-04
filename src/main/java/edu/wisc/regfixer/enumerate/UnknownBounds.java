cc5fb362b9148c3b1a481db9f1213459dcebe5db
isaacev <isaacevavold@gmail.com>
2017-11-04 14:48:39 -0500

    fix typo

--- src/main/java/edu/wisc/regfixer/enumerate/UnknownBounds.java
+++ src/main/java/edu/wisc/regfixer/enumerate/UnknownBounds.java
@@ -6,8 +6,8 @@ public class UnknownBounds extends Bounds implements Unknown {
   // SEE: UnknownChar
   private static Bounds fill = null;
   public static void setFill () { UnknownBounds.setFill(Bounds.atLeast(0)); }
-  public static void setFill (Bounds bounds) { UnknownBounds.bounds = bounds; }
-  public static void clearFill () { UnknownBounds.bounds = null; }
+  public static void setFill (Bounds bounds) { UnknownBounds.fill = bounds; }
+  public static void clearFill () { UnknownBounds.fill = null; }
 
   private UnknownId id;
 
